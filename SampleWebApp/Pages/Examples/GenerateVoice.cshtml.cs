dd4ba1d5085f86da2efe9a2982a313191c43ceac
JasonLWalker <jason@draxjinn.com>
2024-04-10 14:52:27 -0500

    Update generate voice endpoint

--- SampleWebApp/Pages/Examples/GenerateVoice.cshtml.cs
+++ SampleWebApp/Pages/Examples/GenerateVoice.cshtml.cs
@@ -12,34 +12,31 @@ namespace SampleWebApp.Pages.Examples
         [BindProperty]
         public string PromptText { get; set; }
 
-
         public void OnGet()
         {
         }
 
-        public async Task<IActionResult> OnGetVoice(string text)
+        public async Task<IActionResult> OnGetVoice(string text, int voice, string seed, string version)
         {
             string? EncryptionKey = User.Claims.FirstOrDefault(c => c.Type == "Nai.EncryptionKey")?.Value?.ToString();
             string? AccessToken = User.Claims.FirstOrDefault(c => c.Type == "Nai.AccessToken")?.Value?.ToString();
 
             NovelAPI? api = NovelAPI.NewNovelAiAPI(new Structs.AuthConfig() { EncryptionKey = EncryptionKey, AccessToken = AccessToken });
-            var client = api.client;
-            RestRequest request = new RestRequest("ai/generate-voice");
-            request.Method = Method.Get;
-            request.AddParameter("text", text, true);
-            request.AddParameter("voice", "-1", true);
-            request.AddParameter("seed", "Aini", true);
-            request.AddParameter("opus", "false", true);
-            request.AddParameter("version", "v2", true);
-            request.AddHeader("User-Agent", NovelAPI.AGENT);
-            request.AddHeader("Content-Type", "application/json");
-            request.AddHeader("Authorization", "Bearer " + api.keys.AccessToken);
+            var response = await api.GenerateVoiceAsync(new Structs.NaiGenerateVoice()
+            {
+                text = text,
+                voice = voice,
+                seed = seed,
+                opus = false,
+                version = version
+            });
+            if (response.StatusCode == 200)
+            {
 
-            RestResponse response = await client.ExecuteAsync(request);
-            if (response.IsSuccessStatusCode)
-                return new FileContentResult(response.RawBytes ?? new byte[]{}, response.ContentType);
-            
-            return new StatusCodeResult((int)response.StatusCode);
+                return new FileContentResult(response.output, response.ContentType);
+            }
+
+            return new StatusCodeResult(response.StatusCode);
         }
     }
 }
