7fd9847dc04d919648da16fe9b0df5696dbfad0b
Jason Walker <jlwalker@r12mail.sps.org>
2024-04-30 13:55:50 -0500

    Update DataDecode utility to handle keystore and MsgPack decoding

--- SampleWebApp/Pages/Utilities/DataDecode.cshtml.cs
+++ SampleWebApp/Pages/Utilities/DataDecode.cshtml.cs
@@ -1,7 +1,10 @@
+using System.Text;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using net.novelai.api;
+using Newtonsoft.Json;
 
 namespace SampleWebApp.Pages.Utilities
 {
@@ -14,18 +17,46 @@ namespace SampleWebApp.Pages.Utilities
         [BindProperty]
         public string? TextOutput { get; set; } = "";
 
+        [BindProperty]
+        public string MetaKey { get; set; } = "";
+
+        [BindProperty]
+        public IEnumerable<SelectListItem> Keys { get; set; }
+
+
         public void OnGet()
         {
+            string? encryptionKey = User.Claims.FirstOrDefault(c => c.Type == "Nai.EncryptionKey")?.Value?.ToString();
+            string? accessToken = User.Claims.FirstOrDefault(c => c.Type == "Nai.AccessToken")?.Value?.ToString();
+
+            NovelAPI? api = NovelAPI.NewNovelAiAPI(new Structs.AuthConfig() { EncryptionKey = encryptionKey, AccessToken = accessToken });
+            Keys = api.keys.keystore.Select(o => new SelectListItem(o.Key, o.Key));
         }
 
         public async Task<IActionResult> OnPost()
         {
-            string? EncryptionKey = User.Claims.FirstOrDefault(c => c.Type == "Nai.EncryptionKey")?.Value?.ToString();
-            string? AccessToken = User.Claims.FirstOrDefault(c => c.Type == "Nai.AccessToken")?.Value?.ToString();
+            string? encryptionKey = User.Claims.FirstOrDefault(c => c.Type == "Nai.EncryptionKey")?.Value?.ToString();
+            string? accessToken = User.Claims.FirstOrDefault(c => c.Type == "Nai.AccessToken")?.Value?.ToString();
 
-            NovelAPI? api = NovelAPI.NewNovelAiAPI(new Structs.AuthConfig() { EncryptionKey = EncryptionKey, AccessToken = AccessToken });
+            NovelAPI? api = NovelAPI.NewNovelAiAPI(new Structs.AuthConfig() { EncryptionKey = encryptionKey, AccessToken = accessToken });
+            Keys = api.keys.keystore.Select(o => new SelectListItem(o.Key, o.Key));
 
+            TextOutput = api.DecodeData(MetaKey, TextInput);
 
+            if (TextOutput == null)
+            {
+                byte[] data = Convert.FromBase64String(TextInput);
+
+                var unpacker = new NovelAiMsgPackrReader(new MsgUnpackerOptions()
+                {
+                    //MapsAsObjects = true,
+                    UseRecords = false
+                });
+                var json = unpacker.Unpack(data, null);
+
+                TextOutput = JsonConvert.SerializeObject(json ?? new { }, Formatting.Indented);
+
+            }
 
             return Page();
         }
